c8faea7a54b18e305edb15090df7cc5d34698634
DavideFarci <yank92.df@gmail.com>
2023-04-19 17:37:53 +0200

    add while cicle

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano)
 gli elementi della lista individualmente con un ciclo for.
 */
-/*
+
 const arrList = [
 	'pomodori',
 	'funghi',
@@ -15,11 +15,22 @@ const arrList = [
 	'pasta',
 ];
 
-const eleList = document.querySelector('#list');
+const eleList = document.querySelector('.list');
+
 
 // soluzione con il ciclo for
-for (let i = 0; i < arrList.length; i++) {
+// for (let i = 0; i < arrList.length; i++) {
+// 	console.log(arrList[i]);
+// 	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+// }
+
+
+
+// SOLUZIONE CON IL CICLO WHILE
+
+let i = 0;
+while (i < arrList.length) {
 	console.log(arrList[i]);
 	eleList.innerHTML += `<li>${arrList[i]}</li>`;
+    i++
 }
-*/
